93078f232629e4122ba80fd1ed0c2f14d751733e
janso <devjanso@gmail.com>
2016-08-22 18:08:30 +0200

    transfer from approach x,y to angle,edge length

--- vector.js
+++ vector.js
@@ -4,7 +4,8 @@
 var Vector = function(x, y) { this.x = x; this.y = y;	};
 Vector.prototype.clone = function() { return new Vector(this.x, this.y); };
 Vector.prototype.equal = function(v) { return Boolean(this.x == v.x && this.y == v.y); };
-Vector.prototype.difference = function(v) { return new Vector(v.x - this.x, v.y - this.y); };
+Vector.prototype.add = function(v) { this.x += v.x; this.y += v.y; return this; };
+Vector.prototype.get_difference_vector = function(v) { return new Vector(v.x - this.x, v.y - this.y); };
 Vector.prototype.asString = function() { return "["+this.x+","+this.y+"]"; };	
 Vector.prototype.rotate_quarter = function(q) {
 	var x, lq = q % 3;
@@ -28,4 +29,15 @@ Vector.prototype.get_angle = function() {
 		else { return 5; }// -1,-1			
 	}
 }
-		
+Vector.get_vector_for_angle = function(angle) {
+	angle = angle % 8;
+	if( angle == 0) return new Vector(2, 0);
+	else if (angle == 1) return new Vector(1, 1);
+	else if (angle == 2) return new Vector(0, 2);
+	else if (angle == 3) return new Vector(-1, 1);
+	else if (angle == 4) return new Vector(-2, 0);
+	else if (angle == 5) return new Vector(-1, -1);
+	else if (angle == 6) return new Vector(0, -2);
+	else if (angle == 7) return new Vector(1, -1);
+	else return null;
+}
