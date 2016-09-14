f640aee0cd4dbf64776b4975d5186ceb65d1c6d9
Janso <devjanso@gmail.com>
2016-09-14 15:44:42 +0200

    first version of extended combine (for multiple common edges)

--- js/figure.js
+++ js/figure.js
@@ -73,13 +73,13 @@ Figure.prototype.asString = function () {
 	this.path.forEach(function (e) {
 		r = r + e + " "
 	})
-	
+
 	// Q3
 	if (this.q3 == "")
 		r = r + "   no Q3!";
 	else
 		r = r + "   Q3 = " + this.q3;
-	
+
 	// hash
 	if (this.hash == 0)
 		r = r + "   no hash!";
@@ -93,6 +93,31 @@ Figure.prototype.asString = function () {
 		r = r + "   INCONSISTENT!";
 	return r;
 }
+
+Figure.prototype.toShape = function (id, color) {
+	// convert a figure (angle and edge length) to a shape (x,y) to draw it
+	var shape = new Shape(id, color);
+	if (this.path.length == 0)
+		return shape;
+	var vangle = 0;
+	if (this.path[0] == "S")
+		vangle = 1;
+	var v = new Vector(0, 0);
+	for (var i = 1; i < this.path.length; i = i + 2) {
+		vangle = (vangle + 12 - this.path[i]) % 8;
+		v.add(Vector.get_vector_for_angle(vangle));
+		shape.n.push(v.clone());
+	}
+	// resort nodes of shape
+	var n = [],
+	j = 0;
+	for (var i = 0; i < shape.n.length; i++) {
+		j = (i + shape.n.length - 1) % shape.n.length;
+		n.push(shape.n[j]);
+	}
+	shape.n = n;
+	return shape;
+};
 
 Figure.prototype.set_triangle_small = function () {
 	this.path = ['L', 1, 'S', 2, 'S', 1];
@@ -122,6 +147,25 @@ Figure.prototype.set_parallelogram = function () {
 Figure.prototype.set_test = function () {
 	this.path = ['S', 4, 'S', 1, 'L', 3, 'S', 3, 'L', 1, 'S', 4, ];
 	return this;
+};
+
+Figure.prototype.isConsistent = function () {
+	// checks if to total of the inner angles fits to the number of edges and
+	// if the starting point equals the end point
+	if (this.path.length == 0)
+		return true;
+	var vangle = 0;
+	var cangle = 0;
+	var v = new Vector(0, 0);
+	if (this.path[0] == "S")
+		vangle = 1;
+	for (var i = 1; i < this.path.length; i = i + 2) {
+		cangle += this.path[i];
+		vangle = (vangle + 12 - this.path[i]) % 8;
+		v.add(Vector.get_vector_for_angle(vangle));
+	}
+	var r = (v.x == 0 && v.y == 0) && (2 * this.path.length - 8 == cangle);
+	return r;
 };
 
 Figure.prototype.normalize = function () {
@@ -169,42 +213,6 @@ Figure.prototype.normalize = function () {
 	return this;
 };
 
-Figure.prototype.isConsistent = function () {
-	// checks if to total of the inner angles fits to the number of edges and
-	// if the starting point equals the end point
-	if (this.path.length == 0)
-		return true;
-	var vangle = 0;
-	var cangle = 0;
-	var v = new Vector(0, 0);
-	if (this.path[0] == "S")
-		vangle = 1;
-	for (var i = 1; i < this.path.length; i = i + 2) {
-		cangle += this.path[i];
-		vangle = (vangle + 12 - this.path[i]) % 8;
-		v.add(Vector.get_vector_for_angle(vangle));
-	}
-	var r = (v.x == 0 && v.y == 0) && (2 * this.path.length - 8 == cangle);
-	return r;
-};
-
-Figure.prototype.toShape = function (id, color) {
-	// convert a figure (angle and edge length) to a shape (x,y) to draw it
-	var shape = new Shape(id, color);
-	if (this.path.length == 0)
-		return shape;
-	var vangle = 0;
-	if (this.path[0] == "S")
-		vangle = 1;
-	var v = new Vector(0, 0);
-	for (var i = 1; i < this.path.length; i = i + 2) {
-		vangle = (vangle + 12 - this.path[i]) % 8;
-		v.add(Vector.get_vector_for_angle(vangle));
-		shape.n.push(v.clone());
-	}
-	return shape;
-};
-
 Figure.prototype.combine_simple = function (motherindex, childpiece, childindex) {
 	// combine_simple is the first working version of combine.
 	// It is limited to combine figures that share exactly one edge:
@@ -218,45 +226,6 @@ Figure.prototype.combine_simple = function (motherindex, childpiece, childindex)
 	// At the end of the child piece add angles and continue with rest of mother piece.
 
 	// checks
-	if (!(this.path[motherindex] == "S" || this.path[motherindex] == "L"))
-		throw "index not for edge in mother";
-	if (!(childpiece.path[childindex] == "S" || childpiece.path[childindex] == "L"))
-		throw "index not for edge in child";
-	if (this.path[motherindex] != childpiece.path[childindex])
-		throw "incompatible edges";
-
-	// acutal combination
-	if (this.path[motherindex] != childpiece.path[childindex])
-		throw "incompatible edges";
-	var combination = new Figure(this.id, this.color);
-	var mimod = 0;
-	// copy elements of mother to combination
-	for (var mi = (motherindex + 2) % this.path.length, i = 0;
-		i++ < this.path.length - 2;
-		mi = (mi + 1) % this.path.length) {
-		combination.path.push(this.path[mi]);
-	}
-
-	// add angle
-	if (combination.path.length > 1)
-		combination.path[combination.path.length - 1] += childpiece.path[childindex + 1];
-	else
-		mimod = 2;
-	// iterate child-figure (modulo) beginning from link edge
-	for (var ci = (childindex + 2) % childpiece.path.length, i = 0;
-		i++ < childpiece.path.length - 2;
-		ci = (ci + 1) % childpiece.path.length) {
-		combination.path.push(childpiece.path[ci]);
-	}
-	// add angle
-	combination.path[combination.path.length - 1] += this.path[motherindex + 1];
-	if (mimod != 0)
-		combination.path[combination.path.length - 1] += childpiece.path[childindex + 1];
-	return combination;
-};
-
-Figure.prototype.combine = function (motherindex, childpiece, childindex) {
-	// ### continue here!
 	// combine parallelogram L 1 S 3 L 1 S 3 and small triangle L 1 S 2 S 1 with
 	// motherindex 2: L 1 |S| 3 L 1 S 3 and childindex 2: L 1 |S| 2 S 1.
 	// result shall be L 3 S 1 L 4 L 1 S 3.
@@ -300,5 +269,61 @@ Figure.prototype.combine = function (motherindex, childpiece, childindex) {
 	combination.path[combination.path.length - 1] += this.path[motherindex + 1];
 	if (mimod != 0)
 		combination.path[combination.path.length - 1] += childpiece.path[childindex + 1];
+	return combination.normalize();
+};
+
+Figure.prototype.combine = function (motherindex, childpiece, childindex) {
+	// combine parallelogram L 1 S 3 L 1 S 3 and small triangle L 1 S 2 S 1 with
+	// motherindex 2: L 1 |S| 3 L 1 S 3 and childindex 2: L 1 |S| 2 S 1.
+	// result shall be L 3 S 1 L 4 L 1 S 3.
+	// iterate mother piece until link edge (motherindex), add angle and
+	// continue with childpiece (modulo).
+	// At the end of the child piece add angles and continue with rest of mother piece.
+
+	// checks
+	if (!(this.path[motherindex] == "S" || this.path[motherindex] == "L"))
+		throw "index not for edge in mother";
+	if (!(childpiece.path[childindex] == "S" || childpiece.path[childindex] == "L"))
+		throw "index not for edge in child";
+	if (this.path[motherindex] != childpiece.path[childindex])
+		throw "incompatible edges";
+
+	// set start/end indexes
+	var mis = (motherindex + 2) % this.path.length; // start index on mother piece (this), edge after link edge
+	var mie = motherindex; // end index on mother piece
+	var cis = (childindex + 2) % childpiece.path.length; // start index on child piece
+	var cie = childindex; // end index on child piece
+	// ### finalize while... then make testcase
+	while (this.path[mis + 1] == (8 - childpiece.path[cie + 1] ) ) {
+		mis += 2; cie += 2;
+	}
+	
+	// acutal combination
+	var combination = new Figure(this.id, this.color);
+	combination.q3 = this.q3;
+	var mimod = 0;
+
+	// copy elements of mother to combination
+	for (var mi = mis; mi != mie; mi = (mi + 1) % this.path.length) {
+		combination.path.push(this.path[mi]);
+	}
+
+	// calculate first link angle
+	if (combination.path.length > 1) {
+		combination.path[combination.path.length - 1] += childpiece.path[cie + 1];
+	} else
+		mimod = 2;
+
+	// copy elements from child piece to combination
+	for (var ci = cis; ci != cie; ci = (ci + 1) % childpiece.path.length) {
+		combination.path.push(childpiece.path[ci]);
+	}
+
+	// calculate second link angle
+	combination.path[combination.path.length - 1] += this.path[mie + 1];
+	if (mimod != 0)
+		combination.path[combination.path.length - 1] += childpiece.path[cie + 1];
+
+	// normalize
 	return combination.normalize();
 };
